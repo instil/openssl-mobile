02403b24106db185b06b6e4a22b9af463dcd91a0
Paul Shields <paul.shields@instil.co>
2018-07-18 11:08:56 +0100

    Added a script to trigger building OpenSSL for iOS (And eventually Android), and provides a flag to instead download the latest release from GitHub.

---
+++ build-openssl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Simple script to build OpenSSL for iOS (an eventually Android, or optioanlyl pull the binaries for the latest release from GitHub.
+
+REPO_ID="Pkshields/openssl-mobile"
+TEMP_OUTPUT_DIR="/tmp/openssl-mobile"
+TEMP_ZIP_FILE="binary.zip"
+
+getLatestReleaseDownloadUrl() {
+  curl --silent "https://api.github.com/repos/${REPO_ID}/releases/latest" |
+    grep '"browser_download_url":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+if [[ $* == *--no-build* ]]; then
+	echo "Downloading latest OpenSSL release"
+
+	DOWNLOAD_URL=`getLatestReleaseDownloadUrl`
+
+	rm -rf ${TEMP_OUTPUT_DIR} > /dev/null
+	rm -rf include/* > /dev/null
+	rm -rf lib/* > /dev/null
+
+	mkdir -p "${TEMP_OUTPUT_DIR}"
+	wget "${DOWNLOAD_URL}" -O "${TEMP_OUTPUT_DIR}/${TEMP_ZIP_FILE}"
+	unzip "${TEMP_OUTPUT_DIR}/${TEMP_ZIP_FILE}" -d .
+
+	echo "Completed"
+else
+	./build-ios-mac.sh
+fi
