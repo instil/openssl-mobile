7d4485852fade3cb4d401e5194cce5068d7e73da
Paul Shields <paul.shields@instil.co>
2018-09-28 11:08:24 +0100

    Removing absolute directories in the Android cmake file

--- openssl-android.cmake
+++ openssl-android.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 3.4.1)
 
-link_directories(/Users/paulshields/Projects/openssl-mobile/lib/android/${ANDROID_ABI})
-include_directories(/Users/paulshields/Projects/openssl-mobile/include)
+link_directories(${CMAKE_CURRENT_LIST_DIR}/lib/android/${ANDROID_ABI})
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 
 set(OpenSSL ssl crypto)
+
